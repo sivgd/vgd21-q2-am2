63670e9c8a6c05dec1cd2f8550c7a088d6de48df
TheRomanPrince <TheRomanPrince@THEROMANPRINCE-.local>
2021-01-06 16:55:57 -0800

    Update LevelLoader.cs, LevelLoader.cs.meta, and 6 more files...

---
+++ Assets/Eric/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public bool levelFinished = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelFinished)
+        {
+            LoadNextLevel();
+            levelFinished = false;
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+}
