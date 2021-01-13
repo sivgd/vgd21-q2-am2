b5dde82085d79b3838e3f79659a115efbba0f1af
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-01-13 09:19:06 -0800

    hi

---
+++ Assets/Eric/Scripts/UI_HealthHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_HealthHandler : MonoBehaviour
+{
+    public int Health = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayHealth(6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DisplayHealth(int v)
+    {
+        switch (v)
+        {
+            case 6:
+                break;
+            case 5:
+                break;
+            case 4:
+                break;
+            case 3:
+                break;
+            case 2;
+                break;
+            case 1:
+                break;
+        }
+    }
+}
